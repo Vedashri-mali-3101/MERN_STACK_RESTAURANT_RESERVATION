640e9ffda6a72b9c427c81d484714956dd7d1233
Vedashri mali <malivedashri3101@gmail.com>
2026-06-20 20:48:00 +0530

    Initial commit - MERN Restaurant Reservation System

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "RESERVATIONS",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecing to database: ${err}`);
+    });
+};
